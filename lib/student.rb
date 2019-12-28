f4d15b8a47c82583e98d6bd19b5498c2785bd483
Jeshua Hicks <jhicks7884@outlook.com>
2019-12-28 13:38:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,30 +41,23 @@ class Student
   end
   def self.new_from_db(row)
     id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
+   name = row[1]
+   grade = row[2]
+   self.new(id, name, grade)
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE name = ?
-      LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql,name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
+   sql = "SELECT * FROM students WHERE name = ?"
+   DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+ end
 
   def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end
+   sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+   DB[:conn].execute(sql, self.name, self.grade, self.id)
+ end
 
 end
+
 
 
 
